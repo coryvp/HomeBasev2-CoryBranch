c040ec31f39f63f53bc2eff7417dc1894d5f07b3
coryvp <cventres@mit.edu>
2020-11-16 21:20:48 -0500

    Changes to module ui

--- public/modules/modules.js
+++ public/modules/modules.js
@@ -9,23 +9,31 @@ ui.navigation = `
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+            <li class="nav-item">
       <a class="nav-item nav-link" href="#" onclick="loadSignIn()" >Sign In</a>
+      </li>
+      <li class="nav-item">
       <a class="nav-item nav-link" href="#" onclick="loadFindHousing()" >Find Housing</a>
+      </li>
+      <li class="nav-item">
       <a class="nav-item nav-link" href="#" onclick="loadOfferHousing()" >Offer Housing</a>
+      </li>
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarHelp" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Help
+      </a>
+      <div class="dropdown-menu" aria-labelledby="navbarHelp">
+      <a class="dropdown-item" href="#">Contact Us</a>
+      <a class="dropdown-item" href="#">Privacy Policy</a>
+      <a class="dropdown-item" href="#">Terms & Conditions</a>
+      </div>
+  </li>
+      <li class="nav-item">
       <a class="nav-item nav-link" href="#" onclick="loadMyAccount()" >My Account</a>
-    </div>
-    <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarHelp" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Help
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarHelp">
-                <a class="dropdown-item" href="#">Contact Us</a>
-                <a class="dropdown-item" href="#">Privacy Policy</a>
-                <a class="dropdown-item" href="#">Terms & Conditions</a>
-                </div>
-            </li>
+      </li>
+   
   </nav>    
 
 `;
@@ -42,6 +50,49 @@ ui.default = `
     </div>
     <img src="hands.jpeg" class="card-img-top" alt="Home Image">
     </div> 
+`;
+
+ui.myAccount = `
+<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+<div class="card-header">Create Account</div>
+<div class="card-body">
+  <form>
+    <div class="form-group">
+      <label for="firstName"> First Name</label>
+      <input type="text" class="form-control" id="firstName" placeholder="Enter first name">
+    </div>
+    <div class="form-group">
+    <label for="lastName"> Last Name</label>
+    <input type="text" class="form-control" id="lastName" placeholder="Enter last name">
+  </div>
+  <div class="form-group">
+  <label for="phoneNumber"> Phone Number</label>
+  <input type="number" class="form-control" id="phoneNumber" placeholder="Enter phone number">
+</div>
+    <div class="form-group">
+      <label for="email">Email address</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+    <label for="DOB"> Date of Birth</label>
+    <input type="date" class="form-control" id="DOB" placeholder="MM/DD/YYYY">
+  </div>
+  <div class="form-group">
+  <label for="firstName"> First Name</label>
+  <input type="text" class="form-control" id="firstName" placeholder="Enter first name">
+</div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Enter password">
+      </div>
+      <div class="form-group row">
+        <div class="col-sm-10">
+          <button type="submit" class="btn btn-light" onclick="create()">Create account</button>
+        </div>
+      </div>
+  </form>
+</div>
+</div>
 `;
 
 ui.myAccount = `
